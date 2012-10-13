48f4a586b579eba222d5aaf95e8f4af8c6b24dca
= <kaushal.parikh@10gen.com>
2012-10-13 17:12:59 -0400

    Added method to take input and download image.

--- PacMaps/src/Maps/GoogleMapsAPI.java
+++ PacMaps/src/Maps/GoogleMapsAPI.java
@@ -1,26 +1,46 @@
 package Maps;
 
 import java.awt.Image;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
 
 public class GoogleMapsAPI 
 {
-	public Image GetImage(String location)
+	public static void main(String[] args)
 	{
-		String fixedLocation = location.replaceAll("\\s+", " ").replace(" ", "+");
-		Image image = null;
+		String location = "college ave new brunswick";
+		location = location.replaceAll("\\s+", " ").replace(" ", "+");
+		String url = "http://maps.googleapis.com/maps/api/staticmap?center="+ location + "&zoom=13&size=800x600&sensor=false";
 		try 
 		{
-		    URL url = new URL(String.format("http://maps.googleapis.com/maps/api/staticmap?center=%s&zoom=13&size=800x600", fixedLocation));
-		    image = ImageIO.read(url);
-		} 
+			saveImage(url, location + ".jpg");
+		}
 		catch (IOException e) 
 		{
 			System.err.println("No image found for " + location + ".");
 		}
-		return image;
+		
 	}
+	
+	public static void saveImage(String imageUrl, String destinationFile) throws IOException {
+		URL url = new URL(imageUrl);
+		InputStream is = url.openStream();
+		OutputStream os = new FileOutputStream(destinationFile);
+
+		byte[] b = new byte[2048];
+		int length;
+
+		while ((length = is.read(b)) != -1) {
+			os.write(b, 0, length);
+		}
+
+		is.close();
+		os.close();
+	}
+
 }
