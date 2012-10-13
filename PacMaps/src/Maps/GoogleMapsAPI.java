6b4594c02c447c35d6fd7876822a1e79007b6ffe
Steve Mostovoy <stevemostovoysm@gmail.com>
2012-10-13 15:49:54 -0400

    Google Maps API - Needs Testing

--- PacMaps/src/Maps/GoogleMapsAPI.java
+++ PacMaps/src/Maps/GoogleMapsAPI.java
@@ -1,17 +1,26 @@
 package Maps;
 
 import java.awt.Image;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
 
 public class GoogleMapsAPI 
 {
 	public Image GetImage(String location)
 	{
+		String fixedLocation = location.replaceAll("\\s+", " ").replace(" ", "+");
 		Image image = null;
 		try 
-		    URL url = new URL("http://www.yahoo.com/image_to_read.jpg");
+		{
+		    URL url = new URL(String.format("http://maps.googleapis.com/maps/api/staticmap?center=%s&zoom=13&size=800x600", fixedLocation));
 		    image = ImageIO.read(url);
-		} catch (IOException e) {
+		} 
+		catch (IOException e) 
+		{
+			System.err.println("No image found for " + location + ".");
 		}
-		"http://maps.googleapis.com/maps/api/staticmap?center=New+York,NY&zoom=13&size=800x600";
+		return image;
 	}
 }
